021ed7915c1fbe42bec8d2995f033cf037b13ce8
Erik Movsesyan <erik_movsesyan@epam.com>
2024-06-08 21:27:07 +0400

    Code cleanup

--- ConfigurationManagement/ConfigurationEntities/ConfigurationComponent/ConfigurationComponent.cs
+++ ConfigurationManagement/ConfigurationEntities/ConfigurationComponent/ConfigurationComponent.cs
@@ -6,26 +6,17 @@ using System.Reflection;
 using System;
 using ConfigurationManagement.Extensions;
 using System.IO;
-using System.Runtime.Loader;
 
 namespace ConfigurationManagement.ConfigurationEntities
 {
-    public abstract class ConfigurationComponentBase
+    public abstract partial class ConfigurationComponentBase
     {
         private const BindingFlags Binding_Flags = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static;
         private const string PluginsPath = "ConfigurationManagerPlugins";
 
         private readonly Type[] _applicablePropertyTypes = [typeof(int?),  typeof(string), typeof(float?), typeof(TimeSpan)];
-        private readonly List<IConfigurationProvider> _configurationProviders = CreateConfigurationProviders(_loadedAssemblies).ToList();
-
-        private static List<Assembly> _loadedAssemblies = [];
 
         private static readonly EnumerationOptions EnumerationOptions = new() { RecurseSubdirectories = true };
-
-        static ConfigurationComponentBase()
-        {
-            LoadPlugins();
-        }
 
         public void SaveSettings()
         {
@@ -78,61 +69,6 @@ namespace ConfigurationManagement.ConfigurationEntities
                     configProvider.GetType().Name.Equals(propertyConfigProviderType, StringComparison.OrdinalIgnoreCase));
 
             return propertyConfigProvider;
-        }
-
-        private static void LoadPlugins()
-        {
-            string baseDir = AppDomain.CurrentDomain.BaseDirectory;
-            string projectDir = Directory.GetParent(baseDir).Parent.Parent.Parent.FullName;
-            var pluginRelativePath = Path.Combine(projectDir, PluginsPath);
-
-            var plugins = Directory.EnumerateFiles(pluginRelativePath, "*ConfigurationProvider.dll", EnumerationOptions);
-
-            _loadedAssemblies = plugins.Select(LoadPlugin).ToList();
-        }
-
-        private static Assembly LoadPlugin(string pluginPath)
-        {
-            var loadContext = new AssemblyLoadContext(string.Empty);
-            var dependencyResolver = new AssemblyDependencyResolver(pluginPath);
-
-            loadContext.Resolving += (assemblyLoadContext, assemblyName) => 
-            {
-                var assemblyPath = dependencyResolver.ResolveAssemblyToPath(assemblyName);      
-
-                return assemblyPath != null
-                    ? assemblyLoadContext.LoadFromAssemblyPath(assemblyPath) 
-                    : null;
-            };
-
-            return loadContext.LoadFromAssemblyName(new AssemblyName(Path.GetFileNameWithoutExtension(pluginPath)));
-        }
-
-        private static IEnumerable<IConfigurationProvider> CreateConfigurationProviders(IEnumerable<Assembly> assemblies)
-        {
-            foreach (var assembly in assemblies)
-            {
-                int count = 0;
-                foreach (Type type in assembly.GetTypes())
-                {
-                    if (typeof(IConfigurationProvider).IsAssignableFrom(type))
-                    {
-                        if (Activator.CreateInstance(type) is IConfigurationProvider result)
-                        {
-                            count++;
-                            yield return result;
-                        }
-                    }
-                }
-
-                if (count == 0)
-                {
-                    var availableTypes = string.Join(",", assembly.GetTypes().Select(t => t.FullName));
-                    throw new ApplicationException(
-                        $"Can't find any type which implements ${nameof(IConfigurationProvider)} in {assembly} from {assembly.Location}.\n" +
-                        $"Available types are: {availableTypes}");
-                }
-            }
         }
     }
 }
---
+++ ConfigurationManagement/ConfigurationEntities/ConfigurationComponent/ConfigurationComponentBase.LoadPluginAssemblies.cs
@@ -0,0 +1,76 @@
+﻿using ConfigurationManagement.Providers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Loader;
+
+namespace ConfigurationManagement.ConfigurationEntities
+{
+    public abstract partial class ConfigurationComponentBase
+    {
+        private static List<Assembly> _loadedAssemblies = [];
+        private readonly List<IConfigurationProvider> _configurationProviders = CreateConfigurationProviders(_loadedAssemblies).ToList();
+
+        static ConfigurationComponentBase()
+        {
+            LoadPlugins();
+        }
+
+        private static void LoadPlugins()
+        {
+            string baseDir = AppDomain.CurrentDomain.BaseDirectory;
+            string projectDir = Directory.GetParent(baseDir).Parent.Parent.Parent.FullName;
+            var pluginRelativePath = Path.Combine(projectDir, PluginsPath);
+
+            var plugins = Directory.EnumerateFiles(pluginRelativePath, "*ConfigurationProvider.dll", EnumerationOptions);
+
+            _loadedAssemblies = plugins.Select(LoadPlugin).ToList();
+        }
+
+        private static Assembly LoadPlugin(string pluginPath)
+        {
+            var loadContext = new AssemblyLoadContext(string.Empty);
+            var dependencyResolver = new AssemblyDependencyResolver(pluginPath);
+
+            loadContext.Resolving += (assemblyLoadContext, assemblyName) =>
+            {
+                var assemblyPath = dependencyResolver.ResolveAssemblyToPath(assemblyName);
+
+                return assemblyPath != null
+                    ? assemblyLoadContext.LoadFromAssemblyPath(assemblyPath)
+                    : null;
+            };
+
+            return loadContext.LoadFromAssemblyName(new AssemblyName(Path.GetFileNameWithoutExtension(pluginPath)));
+        }
+
+        private static IEnumerable<IConfigurationProvider> CreateConfigurationProviders(IEnumerable<Assembly> assemblies)
+        {
+            foreach (var assembly in assemblies)
+            {
+                int count = 0;
+                foreach (Type type in assembly.GetTypes())
+                {
+                    if (typeof(IConfigurationProvider).IsAssignableFrom(type))
+                    {
+                        if (Activator.CreateInstance(type) is IConfigurationProvider result)
+                        {
+                            count++;
+                            yield return result;
+                        }
+                    }
+                }
+
+                if (count == 0)
+                {
+                    var availableTypes = string.Join(",", assembly.GetTypes().Select(t => t.FullName));
+                    throw new ApplicationException(
+                        $"Can't find any type which implements ${nameof(IConfigurationProvider)} in {assembly} from {assembly.Location}.\n" +
+                        $"Available types are: {availableTypes}");
+                }
+            }
+        }
+    }
+}
